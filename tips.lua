ebeb8fa3ebcdeace6a79a650e36b2759f5cd020d
Trond A Ekseth <troeks@gmail.com>
2012-01-14 22:54:52 +0100

    Don't use select.

--- tips.lua
+++ tips.lua
@@ -1,21 +1,16 @@
--- We like locals
-local select = select
 local GetItemInfo = GetItemInfo
 local GetItemQualityColor = GetItemQualityColor
 
 local origs = {}
 local tips = {GameTooltip, ItemRefTooltip, ShoppingTooltip1, ShoppingTooltip2}
 
---[[
---	Za Warudo!
---]]
 local OnTooltipSetItem = function(self, ...)
 	local orig = origs[self]
 	if(orig) then orig(self, ...) end
 
 	local name, item = self:GetItem()
 	if(item) then
-		local quality = select(3, GetItemInfo(item))
+		local _, _, quality = GetItemInfo(item)
 		if(quality) then
 			local r, g, b = GetItemQualityColor(quality)
 
